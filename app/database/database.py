b3149ace216c0ff54942141242cef32ef9ef89c2
otter.oh <otter.oh@gmail.com>
2025-03-13 17:25:19 +0900

    add branch reg_langgraphs

--- app/database/database.py
+++
@@ -1,6 +0,0 @@
-from app.database.connection import Settings
-
-settings = Settings()
-
-async def init_db():
-    await settings.initialize_database()
--- app/samples/torch_cudas.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("CUDA is available!")
-    print(f"Device Name: {torch.cuda.get_device_name(0)}")
-    print(f"Device Count: {torch.cuda.device_count()}")
-    print(f"Current Device: {torch.cuda.current_device()}")
-else:
-    print("CUDA is not available.")
